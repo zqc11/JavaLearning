ae76da18e820c65627ecc84b1b8f5c4ac413bbf2
zhouqichun <2925009749@qq.com>
2021-01-23 00:00:16 +0800

    2021.1.22

---
+++ src/com/silentSunshine/common/random/RandomTest.java
@@ -0,0 +1,21 @@
+package com.silentsunshine.common.random;
+
+import java.util.Random;
+
+/**
+ * @version 1.0
+ * @auther zhouqichun
+ * @date 2021/1/22 - 01 - 22 - 21:50
+ * @description practice com.silentsunshine.common.random
+ */
+public class RandomTest {
+    public static void main(String[] args) {
+        Random random = new Random();
+        //返回0~bound的随机数
+        System.out.println(random.nextInt(500));
+        //返回高斯分布的double值随机数
+        System.out.println(random.nextGaussian());
+        //返回随机Boolean值
+        System.out.println(random.nextBoolean());
+    }
+}
